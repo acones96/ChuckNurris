6127df2b9e39c81312f059c067e12eaa1037ed22
Adrian Constandse <aconstandse.es@gmail.com>
2023-01-28 02:59:46 -0600

    Autoformat file

--- src/resources/js/Function2005.js
+++ src/resources/js/Function2005.js
@@ -1,7 +1,7 @@
-$( document ).ready(function() {
+$(document).ready(function () {
     xmlhttp = new XMLHttpRequest();
     xmlhttp.open('GET', 'https://api.chucknorris.io/jokes/random/', true);
-    xmlhttp.onreadystatechange = function(){
+    xmlhttp.onreadystatechange = function () {
         let textoChiste = JSON.parse(this.response).value;
         console.log('chiste recibido: ' + textoChiste);
         let ps = document.getElementsByTagName('p');
