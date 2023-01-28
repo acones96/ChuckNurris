e8eb91a4a68008be6f79bc8c9ab45e47fb8aaf13
acones96 <aconstandse.es@gmail.com>
2023-01-27 18:28:01 -0600

    Exercise 2014
    * Generate 10 different jokes in a list using AJAX

---
+++ js/Function2014.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+	for (let index = 0; index < 10; index++) {
+		$.get("https://api.chucknorris.io/jokes/random", (response) => {
+			$("ul").append('<li class= "lead list-group-item">' + response.value + '</li>');
+		})
+	}
+});
