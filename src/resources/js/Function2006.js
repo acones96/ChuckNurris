accad8c31d564ede601cde79f10aa0686f467a30
Adrian Constandse <aconstandse.es@gmail.com>
2023-01-28 02:55:36 -0600

    Modification to project
    * Add navigation bar
    * Format all documents

--- src/resources/js/Function2006.js
+++ src/resources/js/Function2006.js
@@ -1,4 +1,4 @@
-$( document ).ready(function() {
+$(document).ready(function () {
 	$.get("https://api.chucknorris.io/jokes/random", (response) => {
 		let textoChiste = response.value;
 		$('p').text(textoChiste);
