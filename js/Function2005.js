38739f613c735b33d3e1012a0cbc1e7e7f1aac5b
acones96 <aconstandse.es@gmail.com>
2023-01-27 18:14:49 -0600

    Exersice 2005 funtional
    * HTML and JS to print a joke from Chuck Norris API with XMLHTTP para la interaccion con el servidor web.

---
+++ js/Function2005.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.open('GET', 'https://api.chucknorris.io/jokes/random/', true);
+    xmlhttp.onreadystatechange = function(){
+        let textoChiste = JSON.parse(this.response).value;
+        console.log('chiste recibido: ' + textoChiste);
+        let ps = document.getElementsByTagName('p');
+        ps[0].innerHTML = textoChiste;
+    }
+    xmlhttp.send();
+});
