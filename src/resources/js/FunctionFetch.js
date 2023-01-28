b43f4ebd5e98f0d799f21ff6c454f7b21dbf87e8
Adrian Constandse <aconstandse.es@gmail.com>
2023-01-28 00:58:18 -0600

    Funtional Excercise Fetch
    * Use node-fetch to replicate example for 2014

---
+++ src/resources/js/FunctionFetch.js
@@ -0,0 +1,14 @@
+for (let index = 0; index < 10; index++) {
+    fetch('https://api.chucknorris.io/jokes/random')
+    // Handle Response 
+    .then(response => response.json() // Convert into json
+    )
+    // Handle Data
+    .then(response => {
+        $('ul').append('<li class= "lead list-group-item">' + response.value + '</li>');
+    })
+    // Handle Error
+    .catch(error =>
+        console.log("Error encontrado: ", {error})
+    );
+}
