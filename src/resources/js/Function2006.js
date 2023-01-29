e9aa6bb0864f4ed7aadf318cafcb159db65f880f
Adrian Constandse <aconstandse.es@gmail.com>
2023-01-28 23:04:04 -0600

    Delete unnecesary variable

--- src/resources/js/Function2006.js
+++ src/resources/js/Function2006.js
@@ -1,9 +1,7 @@
 $(document).ready(function () {
 	// Makes a GET request to a specific URL and his respective callback
 	$.get("https://api.chucknorris.io/jokes/random", (response) => {
-		// Obtain its key-value and storage it in joke variable
-		let joke = response.value;
-		// Insert the joke in every 'p' tag in the HTML file
-		$('p').text(joke);
+		// Insert the key-value in every 'p' tag in the HTML file
+		$('p').text(response.value);
 	})
 });
