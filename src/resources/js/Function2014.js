b47826d679ed5cebd65c62aa1c0ded55964cc9c3
Adrian Constandse <aconstandse.es@gmail.com>
2023-01-28 23:04:28 -0600

    Add comments

--- src/resources/js/Function2014.js
+++ src/resources/js/Function2014.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
+	// Use a for condition to create 10 different jokes
 	for (let index = 0; index < 10; index++) {
+		// Makes a GET request to a specific URL and his respective callback
 		$.get("https://api.chucknorris.io/jokes/random", (response) => {
+			// Whenever there's an <ol> tag, it will insert the key-value as a <li> tag in the HTML file
 			$("ol").append('<li class= "lead list-group-item">' + response.value + '</li>');
 		})
 	}
