624cae86094aa9359644d0b94ccb58750b9e540e
Adrian Constandse <aconstandse.es@gmail.com>
2023-01-28 22:58:01 -0600

    Add comments

--- src/resources/js/Function2006.js
+++ src/resources/js/Function2006.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
+	// Makes a GET request to a specific URL and his respective callback
 	$.get("https://api.chucknorris.io/jokes/random", (response) => {
+		// Obtain its key-value and storage it in joke variable
 		let joke = response.value;
+		// Insert the joke in every 'p' tag in the HTML file
 		$('p').text(joke);
 	})
 });
