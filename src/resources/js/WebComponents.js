bd1acd97c621f9b04b675b960ed60ff245316bdf
Adrian Constandse <aconstandse.es@gmail.com>
2023-01-31 01:31:46 -0600

    Delete for element

--- src/resources/js/WebComponents.js
+++ src/resources/js/WebComponents.js
@@ -4,27 +4,22 @@ class HTMLChuckNorrisJokeElement extends HTMLElement {
     }
 
     connectedCallback() {
-        for (let index = 0; index < 10; index++) {
-            fetch('https://api.chucknorris.io/jokes/random')
-                // Handle Response 
-                .then(
-                    // Convert the response into json
-                    response => response.json()
-                )
-                // Handle Data
-                .then(joke => {
-                    // Whenever there's an <ol> tag, it will insert the key-value as a <li> tag in the HTML file
-                    $('ol').append('<li class= "list-group-item">' + joke.value + '</li>');
-                    if (index == 9) {
-                        $('ol').append('<div class="text-bg-success p-3">Complete</div>');
-                    }
-                })
-                // Handle Error
-                .catch(error =>
-                    // Will show which was the error in console 
-                    console.log("Error encontrado: ", { error })
-                );
-        }
+        fetch('https://api.chucknorris.io/jokes/random')
+            // Handle Response 
+            .then(
+                // Convert the response into json
+                response => response.json()
+            )
+            // Handle Data
+            .then(joke => {
+                // Whenever there's an <ol> tag, it will insert the key-value as a <li> tag in the HTML file
+                $('ol').append('<li class= "list-group-item">' + joke.value + '</li>');
+            })
+            // Handle Error
+            .catch(error =>
+                // Will show which was the error in console 
+                console.log("Error encontrado: ", { error })
+            );
     }
 }
 
